267df2cc13a93f2b14a6c19e2647a73eaa304f3b
shhady <shhadyse@gmail.com>
2022-04-05 22:50:10 +0300

    primitive-datatypes

---
+++ datatypes.js
@@ -0,0 +1,9 @@
+let name = "Shhady"; //string
+let age = 33; //number
+let obj = {} //object
+let logIn = true; //boolean
+let UserlogedIn = null; //null
+let color; //undefined
+let id = Symbol()
+
+    
